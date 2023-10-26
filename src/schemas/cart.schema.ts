e7b5a644259616274220e8d39e2145211913a7d4
neilen <neilen.monlezun@gmail.com>
2023-10-26 09:44:59 -0300

    cart schema modif

--- src/schemas/cart.schema.ts
+++ src/schemas/cart.schema.ts
@@ -10,16 +10,3 @@ export const CartSchema = new Schema({
   items: [CartItemSchema],
 });
 
-// import * as mongoose from 'mongoose';
-
-// const CartItemSchema = new mongoose.Schema({
-//   productId: { type: mongoose.Schema.Types.ObjectId, ref: 'Product' },
-//   quantity: { type: Number, default: 0 },
-// });
-
-// export const CartSchema = new mongoose.Schema({
-//   userId: { type: mongoose.Schema.Types.ObjectId, ref: 'Users' },
-//   items: [CartItemSchema],
-// });
-
-// export const CartModel = mongoose.model('Cart', CartSchema);
